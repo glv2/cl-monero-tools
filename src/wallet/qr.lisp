9a670f4f43ffb6edef58040dc044fb77fd8f63a6
Guillaume LE VAILLANT <glv@posteo.net>
2017-10-12 09:25:01 +0200

    Refactor QR code decoding

--- src/wallet/qr.lisp
+++ src/wallet/qr.lisp
@@ -17,114 +17,86 @@ a PNG-FILE."
                        :description description)))
     (cl-qrencode:encode-png uri :fpath png-file :mode :byte :pixsize 4)))
 
+(define-foreign-library zbar
+  (t (:default "libzbar")))
+
 (handler-case
-    (progn
-      (define-foreign-library zbar
-        (t (:default "libzbar")))
-
-      (use-foreign-library zbar)
-
-      (defcfun ("zbar_image_create" zbar-image-create) :pointer)
-
-      (defcfun ("zbar_image_destroy" zbar-image-destroy) :void
-        (image :pointer))
-
-      (defcfun ("zbar_image_set_format" zbar-image-set-format) :void
-        (image :pointer)
-        (format :unsigned-long))
-
-      (defcfun ("zbar_image_set_size" zbar-image-set-size) :void
-        (image :pointer)
-        (width :unsigned-int)
-        (height :unsigned-int))
-
-      (defcfun ("zbar_image_set_data" zbar-image-set-data) :void
-        (image :pointer)
-        (data :pointer)
-        (data-length :unsigned-long)
-        (cleanup-handler :pointer))
-
-      (defcfun ("zbar_image_free_data" zbar-image-free-data) :void
-        (image :pointer))
-
-      (defcfun ("zbar_image_first_symbol" zbar-image-first-symbol) :pointer
-        (image :pointer))
-
-      (defcfun ("zbar_image_convert" zbar-image-convert) :pointer
-        (image :pointer)
-        (format :unsigned-long))
-
-      (defcfun ("zbar_processor_create" zbar-processor-create) :pointer)
-
-      (defcfun ("zbar_processor_destroy" zbar-processor-destroy) :void
-        (processor :pointer))
-
-      (defcfun ("zbar_processor_init" zbar-processor-init) :int
-        (processor :pointer)
-        (video-device :pointer :char)
-        (enable-display :int))
-
-      (defcfun ("zbar_process_image" zbar-process-image) :int
-        (processor :pointer)
-        (image :pointer))
-
-      (defcfun ("zbar_symbol_get_data_length" zbar-symbol-get-data-length) :unsigned-int
-        (symbol :pointer))
-
-      (defcfun ("zbar_symbol_get_data" zbar-symbol-get-data) :pointer
-        (symbol :pointer))
-
-      (defcfun ("zbar_symbol_next" zbar-symbol-next) :pointer
-        (symbol :pointer))
-
-      (defun decode-png (file)
-        (flet ((fourcc (fourcc)
-                 (logior (char-code (char fourcc 0))
-                         (ash (char-code (char fourcc 1)) 8)
-                         (ash (char-code (char fourcc 2)) 16)
-                         (ash (char-code (char fourcc 3)) 24))))
-          (pngload:with-png-in-static-vector (png file)
-            (let* ((width (pngload:width png))
-                   (height (pngload:height png))
-                   (data (pngload:data png))
-                   (data-length (length data))
-                   (processor (zbar-processor-create))
-                   (image (zbar-image-create))
-                   result)
-              (zbar-processor-init processor (null-pointer) 0)
-              (zbar-image-set-format image (fourcc "RGB3"))
-              (zbar-image-set-size image width height)
-              (let ((tmp image))
-                (with-foreign-object (raw-data :unsigned-char data-length)
-                  (dotimes (i data-length)
-                    (setf (mem-aref raw-data :unsigned-char i) (aref data i)))
-                  (zbar-image-set-data image raw-data data-length (null-pointer))
-                  (setf image (zbar-image-convert image (fourcc "Y800"))))
-                (zbar-image-destroy tmp))
-              (zbar-process-image processor image)
-              (do ((symbol (zbar-image-first-symbol image)
-                           (zbar-symbol-next symbol)))
-                  ((null-pointer-p symbol))
-                (let* ((data-length (zbar-symbol-get-data-length symbol))
-                       (raw-data (zbar-symbol-get-data symbol))
-                       (data (make-array data-length)))
-                  (dotimes (i data-length)
-                    (setf (aref data i) (mem-aref raw-data :unsigned-char i)))
-                  (setf result (concatenate 'vector result data))))
-              (zbar-image-destroy image)
-              (zbar-processor-destroy processor)
-              (map 'string #'code-char result)))))
-
-      (defun decode-qr-code (png-file)
-        "Return an alist containing the payment information of the QR code
-in a PNG-FILE."
-        (let ((uri (decode-png png-file)))
-          (decode-uri uri))))
-
+    (use-foreign-library zbar)
   (load-foreign-library-error ()
-    (defun decode-qr-code (png-file)
-      "Return an alist containing the payment information of the QR code
-in a PNG-FILE."
-      (declare (ignore png-file))
-      (error "decode-qr-code not supported because the zbar library was not found."))
     (warn "The zbar library was not found.")))
+
+(defun decode-png (file)
+  (if (foreign-library-loaded-p 'zbar)
+      (flet ((fourcc (fourcc)
+               (logior (char-code (char fourcc 0))
+                       (ash (char-code (char fourcc 1)) 8)
+                       (ash (char-code (char fourcc 2)) 16)
+                       (ash (char-code (char fourcc 3)) 24))))
+        (pngload:with-png-in-static-vector (png file)
+          (let* ((width (pngload:width png))
+                 (height (pngload:height png))
+                 (data (pngload:data png))
+                 (data-length (length data))
+                 (processor (foreign-funcall "zbar_processor_create" :pointer))
+                 (image (foreign-funcall "zbar_image_create" :pointer))
+                 result)
+            (foreign-funcall "zbar_processor_init"
+                             :pointer processor
+                             :pointer (null-pointer)
+                             :int 0
+                             :int)
+            (foreign-funcall "zbar_image_set_format"
+                             :pointer image
+                             :unsigned-long (fourcc "RGB3"))
+            (foreign-funcall "zbar_image_set_size"
+                             :pointer image
+                             :unsigned-int width
+                             :unsigned-int height)
+            (let ((tmp image))
+              (with-foreign-object (raw-data :unsigned-char data-length)
+                (dotimes (i data-length)
+                  (setf (mem-aref raw-data :unsigned-char i) (aref data i)))
+                (foreign-funcall "zbar_image_set_data"
+                                 :pointer image
+                                 :pointer raw-data
+                                 :unsigned-int data-length
+                                 :pointer (null-pointer))
+                (setf image (foreign-funcall "zbar_image_convert"
+                                             :pointer image
+                                             :unsigned-long (fourcc "Y800")
+                                             :pointer)))
+              (foreign-funcall "zbar_image_destroy"
+                               :pointer tmp))
+            (foreign-funcall "zbar_process_image"
+                             :pointer processor
+                             :pointer image
+                             :int)
+            (do ((symbol (foreign-funcall "zbar_image_first_symbol"
+                                          :pointer image
+                                          :pointer)
+                         (foreign-funcall "zbar_symbol_next"
+                                          :pointer symbol
+                                          :pointer)))
+                ((null-pointer-p symbol))
+              (let* ((data-length (foreign-funcall "zbar_symbol_get_data_length"
+                                                   :pointer symbol
+                                                   :unsigned-int))
+                     (raw-data (foreign-funcall "zbar_symbol_get_data"
+                                                :pointer symbol
+                                                :pointer))
+                     (data (make-array data-length)))
+                (dotimes (i data-length)
+                  (setf (aref data i) (mem-aref raw-data :unsigned-char i)))
+                (setf result (concatenate 'vector result data))))
+            (foreign-funcall "zbar_image_destroy"
+                             :pointer image)
+            (foreign-funcall "zbar_processor_destroy"
+                             :pointer processor)
+            (map 'string #'code-char result))))
+      (error "QR code decoding not supported because the zbar library was not found.")))
+
+(defun decode-qr-code (png-file)
+  "Return an alist containing the payment information of the QR code
+in a PNG-FILE."
+  (let ((uri (decode-png png-file)))
+    (decode-uri uri)))
