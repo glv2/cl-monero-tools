1a4ddd3d4b7799e0a1cbf8b6b5b276612085b5bf
Guillaume LE VAILLANT <glv@posteo.net>
2019-06-28 10:29:02 +0200

    Use .dir-locals.el file for emacs settings

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((lisp-mode
+  .
+  ((eval . (put 'serialize 'common-lisp-indent-function 2))
+   (eval . (put 'serialize-variant 'common-lisp-indent-function 2))
+   (eval . (put 'deserialize 'common-lisp-indent-function 2))
+   (eval . (put 'deserialize-variant 'common-lisp-indent-function 2)))))
--- emacs/indentation.el
+++
@@ -1,4 +0,0 @@
-(put 'serialize 'common-lisp-indent-function 2)
-(put 'serialize-variant 'common-lisp-indent-function 2)
-(put 'deserialize 'common-lisp-indent-function 2)
-(put 'deserialize-variant 'common-lisp-indent-function 2)
